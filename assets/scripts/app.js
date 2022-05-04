00dbcda8306cdd1ef74194963d763588349697ae
Nathi Geigant <ngeigant@gmail.com>
2022-05-04 18:54:45 +0200

    I have no idea what i'm doing

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,46 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
+function getUserInput() {
+    return parseInt(usrInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDesciption = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDesciption); // from vendor file
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
