84e4a507c726a2aea633a83332faf92eb0b0d714
Nathi Geigant <ngeigant@gmail.com>
2022-05-04 14:22:57 +0200

    Removed some text

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,20 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = result;
-
-let calculationDesciption = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDesciption);
-
-/* Testing out a function before the teacher has fully explained it
-function greetUser(name, age) {
-    alert(`Hi my name is ${name} and I am ${age} years old`);
-}
-
-greetUser('Nathi', 35); */
+addBtn.addEventListener('click', add);
